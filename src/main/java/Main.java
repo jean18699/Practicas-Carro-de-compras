ea186a95e2180cd2c1fd26b9b3d679eed54319a0
jean <jean18699@gmail.com>
2021-05-22 02:49:11 -0400

    Ejercicio 2
    -Se indica la cantidad de párrafos (p) que contiene el documento HTML

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,5 +1,7 @@
 import org.jsoup.Jsoup;
 import org.jsoup.nodes.Document;
+import org.jsoup.nodes.Element;
+import org.jsoup.select.Elements;
 
 import java.io.IOException;
 
@@ -8,11 +10,18 @@ public class Main {
     public static void main(String[] args) throws IOException {
 
 
+
         Document doc = Jsoup.connect("http://help.websiteos.com/websiteos/example_of_a_simple_html_page.htm").get();
+
+        //Variable que almacena todos los elementos que empiezan por la etiqueta p de parrafos
+        Elements parrafos = doc.getElementsByTag("p");
 
         //Contando la cantidad de lineas del recurso (documento HTML)
         System.out.println("Cantidad de lineas de la pagina: " + doc.html().lines().count());
-      
+
+        //Indicar la cantidad de párrafos (p) que contiene el documento HTML
+        System.out.println("Cantidad de parrafos de la pagina: " + parrafos.size());
+
 
     }
 
