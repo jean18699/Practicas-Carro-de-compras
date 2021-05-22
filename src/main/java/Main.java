9d8444205dc64b2194916a95fd4792b604b97631
jean <jean18699@gmail.com>
2021-05-22 03:53:32 -0400

    Ejercicio 3
    Indicar la cantidad de imágenes (img) dentro de los párrafos que
    contiene el archivo HTML.

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -9,20 +9,29 @@ public class Main {
 
     public static void main(String[] args) throws IOException {
 
-
-
-        Document doc = Jsoup.connect("http://help.websiteos.com/websiteos/example_of_a_simple_html_page.htm").get();
+        Document doc = Jsoup.connect("https://developer.mozilla.org/en-US/docs/Learn/HTML/Multimedia_and_embedding/Images_in_HTML").get();
 
         //Variable que almacena todos los elementos que empiezan por la etiqueta p de parrafos
         Elements parrafos = doc.getElementsByTag("p");
 
-        //Contando la cantidad de lineas del recurso (documento HTML)
+        //1. Contando la cantidad de lineas del recurso (documento HTML)
         System.out.println("Cantidad de lineas de la pagina: " + doc.html().lines().count());
 
-        //Indicar la cantidad de párrafos (p) que contiene el documento HTML
+        //2. Indicar la cantidad de párrafos (p) que contiene el documento HTML
         System.out.println("Cantidad de parrafos de la pagina: " + parrafos.size());
 
 
+        //3. Indicar la cantidad de imágenes (img) dentro de los párrafos que contiene el archivo HTML.
+
+        int contadorImg = 0;
+        for(Element parrafo : parrafos)
+        {
+            if(parrafo.getElementsByTag("img").toString().contains("img"))
+            {
+                contadorImg++;
+            }
+        }
+        System.out.println("Cantidad de imagenes dentro de parrafos: " + contadorImg);
     }
 
 }
